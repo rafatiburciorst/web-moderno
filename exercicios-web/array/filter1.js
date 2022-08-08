db6226a982d02f30f3ac0e96f2facba8493763cd
rafatiburciorst <87574111+rafatiburciorst@users.noreply.github.com>
2022-08-07 22:47:49 -0300

    first commit

--- exercicios-web/array/filter1.js
+++ exercicios-web/array/filter1.js
@@ -5,11 +5,34 @@ const produtos = [
     { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
 ]
 
-console.log(produtos.filter(function(p) {
-    return false
-}))
+// console.log(produtos.filter(function(p) {
+//     return false
+// }))
 
-const caro = produto => produto.preco >= 500
-const fragil = produto => produto.fragil
+// const caro = produto => produto.preco >= 500
+// const fragil = produto => produto.fragil
 
-console.log(produtos.filter(caro).filter(fragil))
+// console.log(produtos.filter(caro).filter(fragil))
+
+// const caro = (p) => {
+//     return p.fragil = true
+// }
+
+// console.log(produtos.filter(caro));
+
+const pessoas = [
+    { nome: 'Rafael', idade: 35, adulto: true },
+    { nome: 'Matheus', idade: 15, adulto: false },
+    { nome: 'Daniel', idade: 8, adulto: false },
+    { nome: 'Tatiane', idade: 35, adulto: true },
+]
+
+const adulto = (p) => {
+    return p.adulto
+}
+
+const idade = (i) => {
+    return i.idade > 30
+}
+
+console.log(pessoas.filter(idade));
